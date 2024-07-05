55cf2aa19c8895ce7e1cc958bc11483d6b4e5d6d
Isuru Eranga <isurueranga.0@gmail.com>
2024-07-05 18:30:41 +0530

    service

--- components/section/home/Services/index.tsx
+++ components/section/home/Services/index.tsx
@@ -13,23 +13,23 @@ const Services: FC = () => {
   return (
     <div className=" w-full h-auto bg-primary px-5 py-5">
           <div className=" flex  items-center justify-center flex-wrap">
-              <div  className=" flex flex-col items-center justify-center text-xl px-8 py-5 space-y-4">
-                <MdPayment className=" text-5xl"/>
+              <div  className=" flex flex-col items-center justify-center text-lg lg:text-xl px-8 py-2 lg:py-5 space-y-4">
+                <MdPayment className=" text-xl lg:text-5xl"/>
                 <p>Secure Payments</p>
               </div>
 
-              <div className=" flex flex-col items-center justify-center text-xl px-8 py-5 space-y-4">
-                <TbTruckDelivery className=" text-5xl"/>
+              <div className=" flex flex-col items-center justify-center text-lg  lg:text-xl px-8 py-2 lg:py-5 space-y-4">
+                <TbTruckDelivery className="text-xl  lg:text-5xl"/>
                 <p>Fast Delivery</p>
               </div>
 
-              <div className=" flex flex-col items-center justify-center text-xl px-8 py-5 space-y-4">
-              <MdSentimentVerySatisfied className=" text-5xl"/>
+              <div className=" flex flex-col items-center justify-center text-lg  lg:text-xl px-8 py-2 lg:py-5 space-y-4">
+              <MdSentimentVerySatisfied className="text-xl  lg:text-5xl"/>
               <p>Customer Satisfaction</p>
               </div>
 
-              <div className=" flex flex-col items-center justify-center text-xl px-8 py-5 space-y-4">
-              <MdSupportAgent className=" text-5xl"/>
+              <div className=" flex flex-col items-center justify-center text-lg  lg:text-xl px-8 py-2 lg:py-5 space-y-4">
+              <MdSupportAgent className="text-xl  lg:text-5xl"/>
               <p>Top notch support</p>
               </div>
           </div>
