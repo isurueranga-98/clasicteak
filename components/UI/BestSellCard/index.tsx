568837fcf0ecdb304cae714a8a880b330226b513
Isuru Eranga <isurueranga.0@gmail.com>
2024-07-06 09:47:50 +0530

    bestsell

---
+++ components/UI/BestSellCard/index.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { FC } from "react";
+import Button from "../Button";
+
+type BestSellCardProp = {
+  img: string;
+  link:string;
+  className?: string;
+}
+
+
+const BestSellCard: FC<BestSellCardProp> = (props: BestSellCardProp) => {
+  return(
+    <div className={`${props.className}`}>
+        <div className="relative  ">
+        <Image
+          src={`/${props.img}`}
+          // layout="responsive"
+          
+          // objectFit="cover"
+          alt={props.img}
+          width={500}
+          height={500}
+
+          // layout="fill"
+          objectFit="cover"
+          // objectPosition="left"
+        />
+        <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2 translate-y-1/2">
+          <Button text="Shop Now" behavior="link" href={props.link}/>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default BestSellCard;
--- components/section/home/Category/index.tsx
+++ components/section/home/Category/index.tsx
@@ -1,57 +1,18 @@
 "use client";
 import Image from "next/image";
 import { FC } from "react";
-import c1 from "@/public/c1.png";
-import c2 from "@/public/c2.png";
-import c3 from "@/public/c3.png";
-import Button from "@/components/UI/Button";
+import BestSellCard from "@/components/UI/BestSellCard";
 
 const Category: FC = () => {
   return (
-    <div className="lg:px-10 lg:py-10 flex items-center justify-center space-x-10 px-4 py-4">
-      <div className="relative  ">
-        <Image
-          src={c1}
-          layout="responsive"
-          
-          objectFit="cover"
-          alt="c1"
-        />
-        <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2 translate-y-1/2">
-          <Button text="Shop Now" behavior="link" href="/"/>
-        </div>
-      </div>
-
-      <div className="relative hidden lg:block">
-        <Image
-          src={c2}
-          layout="responsive"
-          
-          objectFit="cover"
-          alt="c1"
-        />
-        <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2 translate-y-1/2">
-          <Button text="Shop Now" behavior="link" href="/"/>
-        </div>
-      </div>
-
-      <div className="relative hidden sm:block">
-        <Image
-          src={c3}
-          layout="responsive"
-          
-          objectFit="cover"
-          alt="c1"
-        />
-        <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2 translate-y-1/2">
-          <Button text="Shop Now" behavior="link" href="/"/>
-        </div>
-      </div> 
+    <div className="lg:px-10 lg:py-10 flex items-center justify-center space-x-10 p-4">
+      <BestSellCard img="c1.png" link="/"/>
+      <BestSellCard img="c2.png" link="/" className=" hidden md:block"/>
+      <BestSellCard img="c3.png" link="/" className=" hidden lg:block"/>
     </div>
   );
 };
 
 export default Category;
-
 
 // absolute   
