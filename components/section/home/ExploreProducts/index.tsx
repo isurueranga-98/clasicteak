5db69b731ac6dbc83abdab841c2401c3729b5cf2
Isuru Eranga <isurueranga.0@gmail.com>
2024-07-06 12:49:55 +0530

    explore

--- components/section/home/ExploreProducts/index.tsx
+++ components/section/home/ExploreProducts/index.tsx
@@ -9,7 +9,7 @@ const ExploreProducts: FC = () => {
   return (
     <div className="w-full flex items-center justify-center md:px-4 lg:px-10">
       <div className="hidden md:block md:w-1/2 lg:w-3/5 h-auto">
-        <div className="relative h-[400px] w-full bg-black">
+        <div className="relative h-[400px] lg:h-[500px] w-full">
           <Image
             src={img3}
             layout="fill"
@@ -19,16 +19,16 @@ const ExploreProducts: FC = () => {
         </div>
       </div>
       <div className="w-full md:w-1/2 lg:w-2/5 h-auto">
-        <div className="relative h-[400px] w-full bg-red-400">
+        <div className="relative h-[400px] lg:h-[500px] w-ful">
           <Image
             src={art}
             layout="fill"
             objectFit="cover"
             alt="art"
           />
-          <div className=" p-4 text-center absolute inset-0 flex flex-col items-center justify-center space-y-5">
-            <p className=" text-3xl text-white">Explore our exclusive Products</p>
-            <p className=" text-2xl font-thin text-white">Bring your vision to life with  hand-curated collections, priced just right</p>
+          <div className=" p-4 text-center absolute inset-0 flex flex-col items-center justify-center space-y-5 lg:space-y-11 xl:space-y-14">
+            <p className=" text-3xl lg:text-4xl xl:text-6xl text-white">Explore our exclusive Products</p>
+            <p className=" text-2xl lg:text-3xl xl:text-4xl font-thin text-white">Bring your vision to life with  hand-curated collections, priced just right</p>
             <Button text="Shop Now" behavior="link" href="/"/>
           </div>
         </div>
