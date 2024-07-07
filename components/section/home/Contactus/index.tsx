13f365bd349bebd757c149bfca9b06206c8b9336
Isuru Eranga <isurueranga.0@gmail.com>
2024-07-07 08:37:30 +0530

    text center

--- components/section/home/Contactus/index.tsx
+++ components/section/home/Contactus/index.tsx
@@ -9,7 +9,7 @@ const ContactUs: FC = () => {
   return (
     <div className="w-full flex items-center justify-center md:px-4 lg:px-10">
       <div className="flex items-center justify-center w-full md:w-1/2 lg:w-3/5 h-[400px] lg:h-[500px] bg-primary">
-        <div id="1" className="flex flex-col items-center justify-center h-full w-full space-y-8">
+        <div id="1" className="flex flex-col items-center text-center justify-center h-full w-full space-y-8">
           <p className="text-white text-4xl">Join Us</p>
           <p className="text-white text-xl font-thin">Sign up for the Newsletter and Receive offers</p>
           <div className="flex flex-col items-center justify-center w-80 space-y-3">
