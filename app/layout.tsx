dd17c97a4c0fac62ef6c6ef1a04420563164f25c
Isuru Eranga <isurueranga.0@gmail.com>
2024-07-15 14:59:05 +0530

    Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import FixedHeader from "@/components/layout/FixedHeader";
+import Footer from "@/components/layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Header/>
         <FixedHeader/>
         <main>{children}</main>
+        <Footer/>
         </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <ProductCard/>
       {/* <CustomerReview/> */}
       <ContactUs/>
-      <Footer/>
+      
    </>
   );
 }
